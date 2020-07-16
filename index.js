299549c9bb85bd82d1f90a801671f10517832ff2
Arthuro Ali Gomez <64677836+Emocrat3@users.noreply.github.com>
2020-07-15 22:23:29 -0400

    Lanzado a heroku api funcionando de react

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var express = require('express');
 var port = process.env.PORT || 3900;
-var app = require('./app');
+var bodyParser = require('body-parser');
+var app = express();
+var path = require('path');
+var ArticleController = require('./controllers/article');
+var multipart = require('connect-multiparty');
+var router = require('./routes/article');
+var md_upload = multipart({uploadDir: './upload/articles'});
+var cors = require('cors');
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
@@ -18,3 +29,13 @@ mongoose.connect('mongodb+srv://emocrat3:emocrat3@reactproyecto.plzbz.mongodb.ne
             });
 
     });
+
+app.get('/api/', ArticleController.probar);
+app.post('/api/save', ArticleController.save);
+app.get('/api/articles/:last?', ArticleController.getArticles);
+app.get('/api/article/:id', ArticleController.getArticle);
+app.put('/api/article/:id', ArticleController.update);
+app.delete('/api/article/:id', ArticleController.delete);
+app.post('/api/upload-image/:id?', md_upload , ArticleController.upload);
+app.get('/api/get-image/:image', ArticleController.getImage);
+app.get('/api/search/:search', ArticleController.search);
