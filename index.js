21577bf8de7444e872fcf9efd59f6b48e0f49b63
Arthuro Ali Gomez <64677836+Emocrat3@users.noreply.github.com>
2020-07-16 17:47:10 -0400

    Cambiando uploader image

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var mongoose = require('mongoose');
 var express = require('express');
 var port = process.env.PORT || 3900;
 var bodyParser = require('body-parser');
+const cloudinary = require('cloudinary');
+cloudinary.config({
+   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+   api_key: process.env.CLOUDINARY_API_KEY ,
+   api_secret:  process.env.CLOUDINARY_API_SECRET 
+});
+
 var app = express();
 var path = require('path');
 var ArticleController = require('./controllers/article');
@@ -18,7 +25,7 @@ app.use(bodyParser.json());
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb+srv://emocrat3:emocrat3@reactproyecto.plzbz.mongodb.net/reactProyecto? retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
     .then(() => {
             console.log('La conexion a mongodb se realizo correctamente!');
 
@@ -36,6 +43,18 @@ app.get('/api/articles/:last?', ArticleController.getArticles);
 app.get('/api/article/:id', ArticleController.getArticle);
 app.put('/api/article/:id', ArticleController.update);
 app.delete('/api/article/:id', ArticleController.delete);
-app.post('/api/upload-image/:id?', md_upload , ArticleController.upload);
+app.post('/api/upload-image/:id?', (req,res)=>{
+       const file = req.files.photo;
+
+       cloudinary.uploader.upload(file.tempFilePath, (err, result) => {
+               res.send({
+                       success: true, 
+                       result
+               })
+       });
+});
+
+
+
 app.get('/api/get-image/:image', ArticleController.getImage);
 app.get('/api/search/:search', ArticleController.search);
